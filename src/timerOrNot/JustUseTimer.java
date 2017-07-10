b4e34df439e6cea059f6edc67a5d4c91aa10f824
guoqinyi <guoqinyi@guoqinyi-PC>
2017-07-10 15:47:26 +0800

    try timer upload

---
+++ src/timerOrNot/JustUseTimer.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package timerOrNot;
+
+/**
+ * @author guoqinyi
+ *
+ */
+public class JustUseTimer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final long timeInternal = 2000;
+		Runnable run = new Runnable() {
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				while(true) {
+					System.out.println("what can i say");
+					try {
+						Thread.sleep(timeInternal);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+				}
+			}
+		};
+		Thread thread = new Thread(run);
+		thread.start();
+	}
+}
