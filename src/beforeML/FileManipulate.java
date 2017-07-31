8c07e5d40425069fa9d0ee7de2c84944a556e3e0
gyge <gyge@DESKTOP-70UCK2K>
2017-07-31 17:50:43 +0800

    修改

--- src/beforeML/FileManipulate.java
+++ src/beforeML/FileManipulate.java
@@ -8,6 +8,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 /**
  * @author gyge
@@ -22,7 +24,8 @@ public class FileManipulate {
 		// TODO Auto-generated method stub
 //		File dir = new File("D:\\video");
 //		distribute(dir);
-		copy();
+//		copy();
+		loadProperties();
 	}
 	
 	private static int x = 0;
@@ -86,5 +89,99 @@ public class FileManipulate {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	/**
+	 * 文件拷贝
+	 */
+	public static void copy1(){
+		String fileName = "熊熊三贱客.We.Bare.Bears.S01E10.720p.HDTV.x264.双语字幕-深影字幕组.mkv";//文件名
+		String sourceDir = "D:\\";//原路径  pwd
+		File sourceExist = new File(sourceDir);//原路径  cd
+		File[] sourceFiles = sourceExist.listFiles();//原路径文件列表  ls
+		System.out.println("sourceExist.getClass = "+sourceExist.getClass());
+		System.out.println("sourceFiles.getClass = "+sourceFiles.getClass());
+		boolean sExist = false;
+
+		for(int j = 0; j < sourceFiles.length; j++){
+			if(fileName.equals(sourceFiles[j].getName())){
+				sExist = true;
+				break;
+			}
+		}
+		if(!sExist){
+			System.out.println("源文件不存在，请检查文件名。");
+		}else{
+			String distanceDir = "D:\\123\\";//目的目录
+			File distanceExist = new File(distanceDir);//目的目录
+			File[] distanceFiles = distanceExist.listFiles();//目的目录文件列表
+			boolean dExist = false;
+			for(int j = 0; j < distanceFiles.length; j++){
+				if(fileName.equals(distanceFiles[j].getName())){
+					dExist = true;
+					break;
+				}
+			}
+			if(dExist){
+				System.out.println("目的文件已存在。");
+			}else{
+				try {
+					File f1 = new File(sourceDir+fileName);
+					File f2 = new File(distanceDir+fileName);
+					FileInputStream in = null;
+					FileOutputStream out = null;
+					long begin = System.currentTimeMillis();
+					long temp = begin;
+					long temp2 = begin;
+					in = new FileInputStream(f1);
+					out = new FileOutputStream(f2);
+					byte[] by = new byte[2097152];
+					int i = 0;
+					while(true){
+						i++;
+						temp = temp2;
+						int res = 0;
+						try {
+							res = in.read(by);
+							if(res == -1){
+								in.close();
+								out.flush();
+								out.close();
+								break;
+							}else{
+								out.write(by);
+								temp2 = System.currentTimeMillis();
+								System.out.println("i = " + i + " &spend = " + (temp2-temp));
+							}
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+					long end = System.currentTimeMillis();
+					System.out.println("end-begin = " + (end-begin));
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	/**
+	 * 读取Property文件
+	 */
+	public static void loadProperties(){
+		InputStream in = FileManipulate.class.getResourceAsStream("D:\\everything.properties");
+		System.out.println(in);
+		Properties p = new Properties();
+		try {
+//			p.load(new FileInputStream(System.getProperty("user.dir")+"\\properties\\everything.properties"));
+			p.load(in);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		p.list(System.out);
 	}
 }
