e2b7b87690ad4421d2c156743e7dcab545533bc9
gyge <gyge@DESKTOP-70UCK2K>
2017-08-17 10:26:43 +0800

    修改

--- src/beforeML/Sorting.java
+++ src/beforeML/Sorting.java
@@ -14,8 +14,14 @@ public class Sorting {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] a = {3,5,4,6,7,8,2,10,17,13,12,11};
-		sort(a);
+		int[] a = {9,27,28,0,90,15,88,36,91,96,44,9};
+//		int[] a = new int[12];
+//		for(int i = 0; i < a.length; i++ ){
+//			a[i] = (int) (Math.random()*100);
+//			System.out.print(a[i] + ",");
+//		}
+//		System.out.println();
+		sort2(a);
 		for(int x : a){
 			System.out.println(x);
 		}
@@ -31,19 +37,48 @@ public class Sorting {
 	}
 	
 	/**
+	 * 选择排序
+	 */
+	public static void sort2(int[] a){
+		for( int i = a.length - 1 ; i > 0 ; i-- ){
+			max2(a,i);
+		}
+	}
+	
+	/**
 	 * 最大值
 	 */
 	public static void max(int[] a, int i){
 		int temp = a[0];
 		int idx = 0;
 		for(int j = 1; j < a.length - i; j++){
-			if(a[j] > temp){
+			if(a[j] > a[idx]){
 				idx = j;
-				temp = a[j];
 			}
 		}
+		temp = a[idx];
 		a[idx] = a[a.length - 1 - i]; 
 		a[a.length - 1 - i] = temp;
+	}
+	
+	/**
+	 * 最大值
+	 */
+	public static void max2(int[] a, int i){
+		int temp = a[0];
+		int idx = 0;
+		for(int j = 1; j < i; j++){
+			if(a[j] > a[idx]){
+				idx = j;
+			}
+		}
+		temp = a[idx];
+		a[idx] = a[i]; 
+		a[i] = temp;
+		for(int x:a){
+			System.out.print(x+",");
+		}
+		System.out.println();
 	}
 
 }
---
+++ src/beforeML/TicketMachine.java
@@ -0,0 +1,78 @@
+/**
+ * 
+ */
+package beforeML;
+
+import java.util.Scanner;
+
+/**
+ * @author gyge
+ *
+ */
+public class TicketMachine {
+	private int price = 100;  //field  字段
+	private int balance;
+	private int total;
+	private String name = "";
+	
+	public TicketMachine(int i) {
+		price = i;
+	}
+	
+	public TicketMachine(String name) {
+		this.name = name;
+	}
+	
+	public TicketMachine(String name, int price) {
+		this(name);
+		this.price = price;
+	}
+
+	public void showPrompt(){
+		System.out.println("Welcome to buy ticket.");
+		System.out.println("The price is " + this.price);
+	}
+	
+	public void insertMoney(int bill){
+		balance += bill;
+		int price = 0;
+		price = price - 10 ;
+		printBalance();
+	}
+	
+	public void printTicket(){
+		if(balance >= price){
+			System.out.println("TICKET");
+			if(balance > price){
+				System.out.println("change is " + (balance - price));
+			}
+			balance -= price;
+		}
+	}
+	
+	public void printBalance(){
+		System.out.println("Welcome to buy ticket.");
+		System.out.println("The price is " + price);
+	}
+	
+	public boolean isEnough(){
+		return balance >= price;
+	}
+	
+	public static void main(String[] args){
+		TicketMachine tm = new TicketMachine("tt",80);
+//		System.out.println(price);
+		System.out.println("name = " + tm.total);
+		int bill;
+		Scanner in = new Scanner(System.in);
+		do{
+			bill = in.nextInt();
+			tm.insertMoney(bill);
+		}while(tm.isEnough());
+		tm.showPrompt();
+		tm.insertMoney(80);
+//		tm.printBalance();
+		tm.insertMoney(20);
+		tm.printTicket();
+	}
+}
