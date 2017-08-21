a3e78ce69ebd1807d392745f6ce34938ba604da6
gyge <gyge@DESKTOP-70UCK2K>
2017-08-21 09:35:33 +0800

    point 1

--- src/beforeML/cdAndDvd/CD.java
+++ src/beforeML/cdAndDvd/CD.java
@@ -3,11 +3,24 @@ package beforeML.cdAndDvd;
 public class CD extends Item {
 
 	private int numOfTracks = 0;
+	private String comment;
+	private String title;
 	
 	public CD(String title, int not) {
 		super(title);
+		System.out.println(3321);
 		numOfTracks = not;
 	}
+	
+	public void print(){
+//		System.out.println(title);
+		super.print();
+		System.out.println(numOfTracks);
+	}
+	
+//	public void title(){
+//		super.title = title;
+//	}
 	
 	public static void main(String[] args){
 		CD cd = new CD("Owl City",7);
