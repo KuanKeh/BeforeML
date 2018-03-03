92c2fcf208827cd2391ebe9f6e4b7169a829c9ff
Kuan <Kuan@Kuan-PC>
2018-03-03 14:36:18 +0800

    routine

--- src/basic/Basic.java
+++ src/basic/Basic.java
@@ -3,6 +3,11 @@
  */
 package basic;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+
 /**
  * @author Kuan
  *
@@ -17,6 +22,9 @@ public class Basic {
 		String $abc = "cccc";
 		String ff = "";
 		System.out.println($abc);
+		List<String> ll1 = new ArrayList<String>();
+		List<String> ll2 = new LinkedList<String>();
+		List<String> ll3 = new Vector<String>();
 	}
 
 }
