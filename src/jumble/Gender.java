8b3078b9e0289c6a42bcf5848691dd82ecf10a00
gyge <gyge@outlook.com>
2018-03-20 21:56:59 +0800

    # WARNING: head commit changed in the meantime
    mergess
    
    routine

---
+++ src/jumble/Gender.java
@@ -0,0 +1,5 @@
+package jumble;
+
+public enum Gender {
+	MALE,FEMALE
+}
---
+++ src/jumble/Newton.java
@@ -0,0 +1,20 @@
+package jumble;
+
+public class Newton {
+	private static int i = 11;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Newton newton = new Newton();
+		System.out.println(newton.i);
+	}
+	
+	private void count(){
+		System.out.println(000);
+		printc();
+	}
+	
+	private void printc(){
+		System.out.println("now in printc");
+		count();
+	}
+}
