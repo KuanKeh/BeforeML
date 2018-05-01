70bb5c4ee94c9bacab0c50645f14fb304825a7ff
GGY <GGY@GGY-PC>
2018-05-01 22:51:31 +0800

    routine

---
+++ src/dataStruct/MyLinkedList.java
@@ -0,0 +1,41 @@
+package dataStruct;
+
+//import java.util.LinkedList.Node;
+
+public class MyLinkedList<E> {
+	int size = 0;
+	
+	Node<E> first;
+	
+	Node<E> last;
+	
+	public MyLinkedList() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	private static class Node<E> {
+        E item;
+        Node<E> next;
+        Node<E> prev;
+
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+	
+	public boolean add(E e) {
+		final Node<E> n = last;
+		final Node<E> newNode = new Node<E>(n,e,null);
+		last = newNode;
+		if(n == null) {
+			first = newNode;
+		}else {
+			n.next = newNode;
+		}
+		size++;
+		return true;
+	}
+
+}
