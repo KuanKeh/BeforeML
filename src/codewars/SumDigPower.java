f228c59db309a007d32e156649a864f2fb3e2565
Kuan <Kuan@Kuan-PC>
2019-02-20 00:10:23 +0800

    r

--- src/codewars/SumDigPower.java
+++ src/codewars/SumDigPower.java
@@ -18,6 +18,9 @@ public class SumDigPower {
 			int count = 1;
 			for(int i = digits.size() - 1; i >= 0; i--) {
 				sum += Math.pow(digits.get(i), count ++);
+				if(sum > l) {
+					break;
+				}
 			}
 			if(sum == l) {
 				list.add(l);
@@ -25,5 +28,28 @@ public class SumDigPower {
 		}
 		
 		return list;
+	}
+	
+	public static List<Long> sumDigPow2(long a, long b){
+		List<Long> list = new LinkedList<Long>();
+		for(long l = a; l <= b; l++) {
+			long temp = l;
+			long sum = 0;
+			int count = 1;
+			for( char c : (""+l).toCharArray()) {
+				sum += Math.pow(c - '0', count ++);
+				if(sum > l) {
+					break;
+				}
+			}
+			if(sum == l) {
+				list.add(l);
+			}
+		}
+		return list;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println('0' - 0);
 	}
 }
