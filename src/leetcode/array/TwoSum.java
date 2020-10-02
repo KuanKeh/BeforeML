4369f62539fbfc49ed88890d701d115e812e27b1
Kuan <gyge@outlook.com>
2020-10-03 00:19:12 +0800

    test12

--- src/leetcode/array/TwoSum.java
+++ src/leetcode/array/TwoSum.java
@@ -24,7 +24,7 @@ public class TwoSum {
         }
         return vector;
     }
-    //test1
+    //test12
     public static void main(String[] args) {
         int[] nums = {3,3};
         int target = 6;
